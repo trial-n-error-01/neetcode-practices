42586efc09917dd6acbd59d8c7aa6a0d0042876e
trial-n-error-01 <tratikd@gmail.com>
2026-09-08 21:25:34 +0800

    Add: top-k-elements-in-list - submission-8

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-8.py
@@ -0,0 +1,24 @@
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        count = {}
+        # Array of lists to store numbers by their frequency
+        freq = [[] for _ in range(len(nums) + 1)]
+        
+        # Step 1: Count frequency of each number
+        for n in nums:
+            count[n] = 1 + count.get(n, 0)
+
+        # Step 2: Map frequency to list of numbers
+        for n, c in count.items():
+            freq[c].append(n)
+
+        # Step 3: Gather top k elements starting from the highest frequency
+        res = []
+        highest_frequency = max(count.values())
+
+        for frequency in range(highest_frequency, 0, -1):
+            res.extend(freq[frequency])
+            if len(res) >= k:
+                return res[:k]
+
+        return res
