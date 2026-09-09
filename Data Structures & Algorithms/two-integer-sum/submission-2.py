893d9f8bd3c79f291226dab6e7caf1339136c764
trial-n-error-01 <tratikd@gmail.com>
2026-09-09 14:37:52 +0800

    Add: two-integer-sum - submission-2

---
+++ Data Structures & Algorithms/two-integer-sum/submission-2.py
@@ -0,0 +1,11 @@
+#Complexity of this one is O()
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        indices = {}
+        for i,n in enumerate(nums):
+            diff = target - n
+            if diff in indices:
+                return [indices[diff], i]
+            indices[n] = i
+        
