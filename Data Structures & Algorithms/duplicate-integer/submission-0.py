425959e6e61e205c1dff017984bb74d77253b968
trial-n-error-01 <tratikd@gmail.com>
2026-09-12 15:33:23 +0800

    Update: duplicate-integer - submission-0

--- Data Structures & Algorithms/duplicate-integer/submission-0.py
+++ Data Structures & Algorithms/duplicate-integer/submission-0.py
@@ -1,9 +1,3 @@
 class Solution:
     def hasDuplicate(self, nums: List[int]) -> bool:
-        hashMap = {}
-        for i in nums:
-            hashMap[i]= hashMap.get(i,0)+1
-            if(hashMap[i])>1:
-                return True
-        
-        return False
+        return len(nums)!= len(set(nums))
