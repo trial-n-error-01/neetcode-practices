9f2eb35e3832e46ade00ad980d1331fd63ff35f9
trial-n-error-01 <tratikd@gmail.com>
2026-09-19 23:45:33 +0800

    Add: binary-search - submission-6

---
+++ Data Structures & Algorithms/binary-search/submission-6.py
@@ -0,0 +1,17 @@
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        # 41 ms
+        def binary(l, r):
+            if l > r:
+                return -1
+            
+            mid = (r + l) // 2
+            
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] < target:
+                return binary(mid + 1, r)
+            else:
+                return binary(l, mid - 1)
+        
+        return binary(0, len(nums) - 1)
