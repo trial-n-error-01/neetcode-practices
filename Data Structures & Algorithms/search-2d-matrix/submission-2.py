7b33d859ffa2879c50069fb470406dc1c59f9528
trial-n-error-01 <tratikd@gmail.com>
2026-09-19 23:57:28 +0800

    Add: search-2d-matrix - submission-2

---
+++ Data Structures & Algorithms/search-2d-matrix/submission-2.py
@@ -0,0 +1,14 @@
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        for row in matrix:
+            if (target>=row[0] and target<=row[-1]):
+                l, r = 0, len(row)-1
+                while(l<=r):
+                    mid = (r+l)//2
+                    if(row[mid]==target):
+                        return True
+                    elif(row[mid]<target):
+                        l=mid+1
+                    else:
+                        r=mid-1
+        return False
