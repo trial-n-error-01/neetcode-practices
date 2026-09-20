64a44ff3a6fee9d1acf196f211dfb1bc05ef2da1
trial-n-error-01 <tratikd@gmail.com>
2026-09-20 13:09:15 +0800

    Add: majority-element - submission-2

---
+++ Data Structures & Algorithms/majority-element/submission-2.py
@@ -0,0 +1,12 @@
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        #same loop
+        hashMap = defaultdict(int)
+        ans, maxC = 0,0
+        for number in nums:
+            hashMap[number] = hashMap.get(number,0)+1
+            if(maxC<hashMap[number] ):
+                maxC= hashMap[number] 
+                ans = number
+        
+        return ans
